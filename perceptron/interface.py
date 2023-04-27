f8ba1bf5fa4f150cb1be45516627c876827e944c
Andrei <andreisva2023@gmail.com>
2023-04-26 19:30:24 -0700

    only paint when mouse is moving

--- perceptron/interface.py
+++ perceptron/interface.py
@@ -8,14 +8,25 @@ def main():
 
     grid = np.zeros(28 * 28).reshape((28, 28))
     brush = np.array([
-        [1, 3, 1],
-        [2, 5, 2],
-        [1, 3, 1]])
+        [8, 15, 8],
+        [15, 20, 15],
+        [8, 15, 8]])
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    grid.fill(0)
+            if event.type == pygame.MOUSEMOTION:
+                if pygame.mouse.get_pressed()[0] == True:
+                    x, y = pygame.mouse.get_pos()
+                    x_cell = (x // 10) - 1
+                    y_cell = (y // 10) - 1
+                    if x_cell >= 0 and x_cell < 26 and y_cell >= 0 and y_cell < 26:
+                        grid[y_cell:y_cell + brush.shape[0], x_cell:x_cell + brush.shape[1]] += brush
+                        grid = np.clip(grid, 0, 255)
 
         screen.fill("black")
 
@@ -23,13 +34,7 @@ def main():
             for j, cell in enumerate(row):
                 pygame.draw.rect(screen, (cell, cell, cell), pygame.Rect((j * 10, i * 10), (10, 10)))
 
-        if pygame.mouse.get_pressed()[0] == True:
-            x, y = pygame.mouse.get_pos()
-            x_cell = (x // 10) - 1
-            y_cell = (y // 10) - 1
-            if x_cell >= 0 and x_cell < 26 and y_cell >= 0 and y_cell < 26:
-                grid[y_cell:y_cell + brush.shape[0], x_cell:x_cell + brush.shape[1]] += brush
-            grid = np.clip(grid, 0, 255)
+
                 
         # forward propagate
 
---
+++ perceptron/perceptron.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+def feedforward(w1):
+    
+
+def predict(m_in):
+    pass
