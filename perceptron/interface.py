1d5228f272359288ab37166a41b16679a752c66f
Andrei <andreisva2023@gmail.com>
2023-11-04 13:32:58 -0700

    load training data

--- perceptron/interface.py
+++ perceptron/interface.py
@@ -1,4 +1,5 @@
 import pygame
+from . import perceptron
 import numpy as np
 
 def paint(grid, brush):
@@ -13,7 +14,7 @@ def main():
     screen = pygame.display.set_mode((280, 280))
     pygame.display.set_caption("pygame-perceptron UI")
 
-    grid = np.zeros(28 * 28).reshape((28, 28))
+    grid = np.zeros((perceptron.CANVAS_WIDTH, perceptron.CANVAS_HEIGHT))
     brush = np.array([
         [8, 15, 8],
         [15, 20, 15],
@@ -39,7 +40,10 @@ def main():
                 pygame.draw.rect(screen, (cell, cell, cell), pygame.Rect((j * 10, i * 10), (10, 10)))
                 
         # forward propagate
-
+        
         pygame.display.flip()
 
     pygame.quit()
+
+if __name__ == "__main__":
+    main()
--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,10 +1,43 @@
 import numpy as np
 
+CANVAS_WIDTH = 28
+CANVAS_HEIGHT = 28
+
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-def feedforward(w1):
+class PerceptronNetwork:
+    def __init__(self):
+        # inputs
+        self.w0 = np.zeros(CANVAS_WIDTH * CANVAS_HEIGHT)
     
+        # hidden layers
+        self.w1 = np.random.rand(CANVAS_WIDTH * CANVAS_HEIGHT * 2)
+        self.w2 = np.random.rand(CANVAS_WIDTH * CANVAS_HEIGHT * 2)
+        self.w3 = np.random.rand(CANVAS_WIDTH * CANVAS_HEIGHT)
 
-def predict(m_in):
-    pass
+        # output layer
+        self.w4 = np.zeros(10)
+
+def load_dataset(path):
+    dataset = []
+    with open(path, "r") as f:
+        data = f.readlines()
+        for i in range(1, len(data)):
+            cells = data[i].rstrip().split(",")
+            dataset.append({
+                "value" : cells[0],
+                "canvas": np.array(cells[1:])
+            })
+    return dataset
+
+def train():
+    print("loading dataset...")
+    dataset = load_dataset("mnist_train.csv")
+    print("dataset loaded.")
+
+
+
+
+if __name__ == "__main__":
+    train()
